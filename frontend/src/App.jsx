24dc791bea7b8246dd152719a57865c70027f2eb
김강연 <kangyeon9525@gmail.com>
2026-03-28 10:37:02 +0900

    feat: 하단바 컴포넌트 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
