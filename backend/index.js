cc7706eb31c22872224bcf5f7937457a9f613530
김강연 <kangyeon9525@gmail.com>
2026-03-29 23:34:33 +0900

    feat: 백엔드 User스키마 생성

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB와 연결이 되었습니다."))
+  .catch((error) => console.log("MongoDB와 연결에 실패했습니다: ", error));
 
 app.listen(PORT, () => {
   console.log("Server is running");
